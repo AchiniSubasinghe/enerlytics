117127a0e7c56b3d5feaa3631da9b661c5fd4a7e
AchiniSubasinghe <achinimaheshika12@gmail.com>
2026-01-08 23:28:20 +0530

    simplified top nav

--- src/components/global/TopNav.jsx
+++ src/components/global/TopNav.jsx
@@ -2,18 +2,16 @@
 
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
-export default function TopNav({ title, items = [] }) {
+export default function TopNav({ title }) {
   return (
     <div className="w-full bg-white border-b p-4 flex justify-between items-center">
       <div className="flex items-center space-x-3">
         <SidebarTrigger />
         <h2 className="text-xl font-semibold">{title}</h2>
       </div>
-<div>
-  <div className="w-10 h-10 rounded-full bg-accent flex items-center justify-center cursor-pointer border-2">
-    <h2>S</h2>
-  </div>
-</div>
+      <div className="w-10 h-10 rounded-full bg-accent flex items-center justify-center border-2">
+        <span className="font-semibold">U</span>
+      </div>
     </div>
   );
 }
