1cc209108d208d7e6476af65e4c8d0eb3cf08a3c
sith <skdev.techno@gmail.com>
2026-01-09 16:06:58 +0530

    fix

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 export const db = await mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'utility_management_system'
 });
